d99cc7663c90f576cc0a20fe6c0dd5446472deb1
tim <tim>
2021-03-31 23:41:06 +0100

    second

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main className="App-main">
+        <h1> timmyc.dev</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
